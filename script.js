aa9ce7ba0a4ac68f693ea217793cf6a110d2ce6d
thatonerookiedev <shatoriag01@gmail.com>
2022-08-16 20:18:23 -0500

    ii

--- script.js
+++ script.js
@@ -8,13 +8,61 @@ document.addEventListener('keydown', (event) => {
         audio.volume = 1.0;
         audio.play();
         /*-------------------*/
-
+        bass.style.width = '16rem';
+        bass.style.height = '16rem';
+        bass.style.borderColor = 'gold';
 
 
         /*-----------------------*/
         console.log('pressed');
     }
 }, false);
+
+document.addEventListener('keyup', (event) => {
+    var name = event.key;
+    if (name === ' ') {
+
+        /*-------------------*/
+        bass.style.width = '15rem';
+        bass.style.height = '15rem';
+        bass.style.borderColor = 'Black';
+
+
+        /*-----------------------*/
+        console.log('back');
+    }
+}, false);
+
+bass.addEventListener('click', function() {
+    var audio = new Audio("bass.mp3")
+    volume = 1.0;
+    audio.play();
+})
+
+
+
+document.addEventListener('keydown', (event) => {
+    var name = event.key;
+    if (name === 'c') {
+        var audio = new Audio("bass.mp3")
+        audio.volume = 1.0;
+        audio.play();
+        /*-------------------*/
+        bass.style.width = '16rem';
+        bass.style.height = '16rem';
+        bass.style.borderColor = 'gold';
+
+
+        /*-----------------------*/
+        console.log('pressed');
+    }
+}, false);
+
+
+
+
+
+
 /*-------------------------------------*/
 /*-------------------------------------*/
 
